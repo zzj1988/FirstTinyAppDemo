9c4fcf0fcd70809c3e97a7f151cad2288b175519
zzj <184195794@qq.com>
2018-08-08 17:31:27 +0800

    绘制地图

--- pages/mainGame/mainGame.js
+++ pages/mainGame/mainGame.js
@@ -5,7 +5,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
   },
 
   /**
@@ -13,13 +12,35 @@ Page({
    */
   onLoad: function (options) {
   
-    var ctx = wx.createCanvasContext('can');
-    var sn = [42, 41], dz = 43, fx = 1, n;
-    ctx.setFillStyle('#666'); //设置颜色
+    this.draw();
+    document.onkeydown = function (e) {
+      fx = sn[1] - sn[0] == (n = [-1, -20, 1, 20][(e || event).keyCode - 37] || fx) ? fx : n
+    };
+    !function () {
+      sn.unshift(n = sn[0] + fx);
+      if (sn.indexOf(n, 1) > 0 || n < 0 || n > 399 || fx == 1 && n % 20 == 0 || fx == -1 && n % 20 == 19)
+        return alert("GAME OVER");
+      draw(n, "Lime");
+      if (n == dz) {
+        while (sn.indexOf(dz = ~~(Math.random() * 400)) >= 0);
+        draw(dz, "Yellow");
+      } else
+        draw(sn.pop(), "Black");
+      setTimeout(arguments.callee, 130);
+    }();
+    
+  },
 
-    //绘制图形，显示时的左上角（x,y）坐标，图形的宽高（设置成18可能是因为存在边框）
-    ctx.fillRect(t % 20 * 20 + 1, ~~(t / 20) * 20 + 1, 18, 18);
-    ctx.draw();
+   draw:function () {
+
+     var t = 10;
+     var ctx = wx.createCanvasContext('can');
+     var sn = [42, 41], dz = 43, fx = 1, n;
+     ctx.setFillStyle('#666'); //设置颜色
+
+     //绘制图形，显示时的左上角（x,y）坐标，图形的宽高（设置成18可能是因为存在边框）
+     ctx.rect(t % 20 * 20 + 1, ~~(t / 20) * 20 + 1, 18, 18);
+     ctx.fill();
   },
 
   /**
