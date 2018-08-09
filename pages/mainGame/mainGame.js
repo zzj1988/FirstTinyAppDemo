48639b8766c983010b2cc78b37edc31b7ef4d7b2
zzj <184195794@qq.com>
2018-08-09 17:32:33 +0800

    1231213

--- pages/mainGame/mainGame.js
+++ pages/mainGame/mainGame.js
@@ -5,44 +5,89 @@ Page({
    * 页面的初始数据
    */
   data: {
+     t : 0,
+     sn : [42, 41], 
+     dz : 43,
+     fx : 1,
+     n : 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
-    this.draw();
-    document.onkeydown = function (e) {
-      fx = sn[1] - sn[0] == (n = [-1, -20, 1, 20][(e || event).keyCode - 37] || fx) ? fx : n
-    };
-    !function () {
-      sn.unshift(n = sn[0] + fx);
-      if (sn.indexOf(n, 1) > 0 || n < 0 || n > 399 || fx == 1 && n % 20 == 0 || fx == -1 && n % 20 == 19)
+
+   var t = this.t;
+   var sn = this.sn;
+   var dz = this.dz;
+   var fx = this.fx;
+   var n = this.n;
+    // this.draw();
+    !function() {
+    sn.unshift(n = sn[0] + fx);
+    if (sn.indexOf(n, 1) > 0 || n<0||n>399 || fx == 1 && n % 20 == 0 || fx == -1 && n % 20 == 19)
         return alert("GAME OVER");
-      draw(n, "Lime");
-      if (n == dz) {
+    draw(n, '#666');
+    if (n == dz) {
         while (sn.indexOf(dz = ~~(Math.random() * 400)) >= 0);
-        draw(dz, "Yellow");
-      } else
-        draw(sn.pop(), "Black");
-      setTimeout(arguments.callee, 130);
+      draw(dz, '#777');
+    } else
+    {
+      draw(sn.pop(), '#888');
+    }
+    setTimeout(arguments.callee, 130);
+
+      this.t = t;
+      this.sn = sn;
+      this.dz = dz;
+      this.fx = fx;
+      this.n = n;
+      
     }();
-    
   },
 
-   draw:function () {
+  draw:function (t,c) {
+    var t = this.t;
+    var sn = this.sn;
+    var dz = this.dz;
+    var fx = this.fx;
+    var n = this.n;
 
-     var t = 10;
-     var ctx = wx.createCanvasContext('can');
-     var sn = [42, 41], dz = 43, fx = 1, n;
-     ctx.setFillStyle('#666'); //设置颜色
+    var ctx = wx.createCanvasContext('canvas');
+     ctx.setFillStyle(c); //设置颜色
 
      //绘制图形，显示时的左上角（x,y）坐标，图形的宽高（设置成18可能是因为存在边框）
      ctx.rect(t % 20 * 20 + 1, ~~(t / 20) * 20 + 1, 18, 18);
      ctx.fill();
+    this.t = t;
+    this.sn = sn;
+    this.dz = dz;
+    this.fx = fx;
+    this.n = n;
   },
 
+  clickDirect: function(event) {
+    console.log(event);
+    var keycode;
+    if (event.currentTarget.id == "upBtn"){
+      console.log("up");
+      keycode = 38;
+    } 
+    else if (event.currentTarget.id == "leftBtn"){
+      console.log("left");
+      keycode = 37;
+    }
+    else if (event.currentTarget.id == "rightBtn") {
+      console.log("right");
+      keycode = 39;
+    }
+    else if (event.currentTarget.id == "downBtn") {
+      console.log("down");
+      keycode = 40;
+    }
+    this.fx = this.sn[1] - this.sn[0] == (this.n = [-1, -20, 1, 20][keyCode - 37] || this.fx) ? this.fx : this.n;
+    console.log(this.fx);
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -91,4 +136,5 @@ Page({
   onShareAppMessage: function () {
   
   }
+
 })
